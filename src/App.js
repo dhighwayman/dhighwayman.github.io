0c9ea61d31406db19e868cd77450ee3dd48c2506
David Santamaria <d.highwayman@gmail.com>
2021-03-31 16:22:51 +0200

    Updated experience

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
@@ -39,6 +41,8 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Portfolio data={this.state.resumeData.portfolio}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
